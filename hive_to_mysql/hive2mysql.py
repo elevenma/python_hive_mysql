b8bb5f46885478e62d702571c361185a7b67e52c
hanyan <hanyan1@staff.weibo.com>
2016-05-24 00:16:19 +0800

    index

--- hive_to_mysql/hive2mysql.py
+++ hive_to_mysql/hive2mysql.py
@@ -8,8 +8,17 @@ def process(str_line):
  #   print json.dumps(decoded,sort_keys=True,indent=1)
     imp_groups = value[4] + "("
     product = ""
+    length = 0
+    if(value[5] != "null"):
+        length = len(decoded)
+        for i in range(0,len(decoded),1):
+            length += len(decoded[i]['tmeta_l3'])
+    
     mid = ""
+    mk = ["0"]*length
     if(decoded):
+        ll = 0
+        start = 0
         for i in range(0,len(decoded),1):
             if(decoded[i]['product']):
                 product = product + decoded[i]['product'] + "/"
@@ -21,14 +30,31 @@ def process(str_line):
                 imp_groups = imp_groups + 'no_imp_groups' + "/"
             if(decoded[i]['tmeta_l3']):
                 for j in range(0,len(decoded[i]['tmeta_l3'])):
-                    if(decoded[i]['tmeta_l3'][j]['itemid']):
-                        mid = mid + decoded[i]['tmeta_l3'][j]['itemid']+ ","
+                    if "itemid" in decoded[i]['tmeta_l3'][j]:
+                        index = int(decoded[i]['tmeta_l3'][j]['loc'])-1
+                        if(decoded[i]['tmeta_l3'][j]['itemid']):
+                            mk[index+start] = decoded[i]['tmeta_l3'][j]['itemid']
+                        else:
+                            mk[index+start] = "no_mid"
                     else:
-                        mid = mid + "no_mid" + ","
-                mid = mid[:-1] + "|"          
+                        index = int(decoded[i]['tmeta_l3'][j]['loc'])-1
+                        mk[index+start] = 'no_mid'
+                    ll += 1
+                mk[ll] = "|"
+                ll += 1
+                start = ll
             else:
-                mid = mid + "no_mid" + "|"
-
+                mk[ll] = 'no_mid'
+                ll += 1
+                mk[ll] = "|"
+                ll += 1
+                start = ll
+ 
+    for mm in mk:
+        if(mm != '|'):
+            mid +=  mm + ','
+        else:
+            mid = mid[:-1] + '|'
 
     if(value[4] == "0"):
         imp_groups = "0"
@@ -56,8 +82,9 @@ try:
         for line in lines:
             if(line):
                 (reqtime,unread_status,available_pos,imp_groups,product,mid,service_name) = process(line)
-                sql_str = 'insert into hy_uid_stats(uid,reqtime,stat_date,unread_status,service_name,available_pos,imp_groups,product,mid) values (\''+ data["uid"] + '\',\''+ reqtime + '\',\'' + data["stat_date"] + '\',\'' + unread_status + '\',\'' + service_name + '\',\'' + available_pos + '\',\'' + imp_groups + '\',\'' + product + '\',\'' + mid + '\')'
-                cur.execute(sql_str)
+                if(unread_status != "0" or available_pos != "0" or imp_groups != "0" or product !='' or mid != ''):
+                    sql_str = 'insert into hy_uid_stats(uid,reqtime,stat_date,unread_status,service_name,available_pos,imp_groups,product,mid) values (\''+ data["uid"] + '\',\''+ reqtime + '\',\'' + data["stat_date"] + '\',\'' + unread_status + '\',\'' + service_name + '\',\'' + available_pos + '\',\'' + imp_groups + '\',\'' + product + '\',\'' + mid + '\')'
+                    cur.execute(sql_str)
 
         hy_str = "update hy_hive_mysql set pstat_status=0,rstat_status=1 where uid='" + data['uid'] + "' and stat_date='" + data['stat_date']+ "' and service_name='" + data['service_name'] + "'"
         cur.execute(hy_str)
@@ -67,5 +94,3 @@ try:
     conn.close()
 except MySQLdb.Error,e:
     print "Mysql Error %d: %s" % (e.args[0],e.args[1])
-
-
